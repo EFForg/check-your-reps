e78fe666d6e5473672994d06649d8f67c032e0a9
vivian <vivian@eff.org>
2018-02-05 11:06:56 -0800

    Users add scores, not reps

--- app/admin/reps.rb
+++ app/admin/reps.rb
@@ -1,23 +1,4 @@
 ActiveAdmin.register Rep do
-  permit_params :name, :bioguide_id,
-    score_attributes: [:position, :source_url]
-
-  controller do
-    def new
-      @rep = Rep.new
-      @rep.build_score
-    end
-  end
-
-  form do |f|
-    f.inputs do
-      f.input :name
-      f.input :bioguide_id, label: "Bioguide ID"
-      f.input :position
-      f.input :source_url
-    end
-
-    f.actions
-  end
-
+  actions :show
+  menu false
 end
---
+++ app/admin/scores.rb
@@ -0,0 +1,4 @@
+ActiveAdmin.register Score do
+  permit_params :position, :source_url, :rep_id
+
+end
