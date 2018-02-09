a0336d5f2d6bbd9132135bf717db248553184597
Vivian Brown <vivian@eff.org>
2018-02-08 16:55:09 -0800

    Get district from Smarty Streets

---
+++ lib/smarty_streets.rb
@@ -0,0 +1,31 @@
+require "rest_client"
+
+module SmartyStreets
+  def self.get_congressional_district(street, zipcode)
+    url = "https://api.smartystreets.com/street-address"
+    res = post(url, base_params.merge(street: street, zipcode: zipcode))
+    if res && res.first
+      district = res.first.dig("metadata", "congressional_district")
+      district = "0" if district == "AL"
+      [res.first["components"]["state_abbreviation"], district]
+    end
+  end
+
+  private
+  def self.post(url, params)
+    begin
+      res = JSON.parse RestClient.get("#{url}?#{params.to_query}")
+      return res
+    rescue => e
+      Rails.logger.error "#{ e } (#{ e.class })!"
+      return false
+    end
+  end
+
+  def self.base_params
+    {
+      "auth-id" => Rails.application.secrets.smarty_streets_id,
+      "auth-token" => Rails.application.secrets.smarty_streets_token
+    }
+  end
+end
