570f6d7019e40cba5837c3b927c51d0c1e37e58a
Schatzkin <schatzkin@eff.org>
2018-02-08 16:58:09 -0800

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: "home#index"
+
+  get "/about", as: :about, to: "about#index"
 end
