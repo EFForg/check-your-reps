03b5a83fc047d87583a99135140f7d604d98c672
Vivian Brown <vivian@eff.org>
2018-02-12 10:34:43 -0800

    Render local scores

--- spec/requests/scores_spec.rb
+++ spec/requests/scores_spec.rb
@@ -1,23 +1,33 @@
 require "rails_helper"
 
 describe "Public access to scores", type: :request do
-  let(:congress_member) { FactoryBot.create(:congress_member, 
-                                            state: "CA",
-                                            district: "14") }
+  let(:score) do
+    FactoryBot.create(:score, congress_member: FactoryBot.create(:senator,
+                                                                 state: "CA"))
+  end
 
   let(:address) do
     { street: "815 Eddy Street", zipcode: "94109" }
   end
 
   before(:each) do
+    score
+
     allow(SmartyStreets).to receive(:get_congressional_district).
       with(address[:street], address[:zipcode]).
       and_return([ "CA", "14" ])
   end
 
-  it "gets a scorecard by address" do
-    congress_member
-    get "/scores/lookup", params: address
-    expect(response.status).to eq(200)
+  describe "score lookup by address" do
+    it "looks up scorecards by address" do
+      get "/scores/lookup", params: address
+      expect(response.body).to include("Buffy Summers")
+    end
+
+    it "returns scorecards to javascript as json" do
+      get "/scores/lookup", params: address.merge({format: :json})
+      expect(response.status).to eq(200)
+      # expect(response.body).to include("Buffy Summers")
+    end
   end
 end
