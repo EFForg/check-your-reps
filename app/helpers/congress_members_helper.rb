b69f2cde1a10d25ca9cc4e465577c327e376d6fe
Schatzkin <schatzkin@eff.org>
2018-02-26 12:45:54 -0800

    trying to get rep title to show up

--- app/helpers/congress_members_helper.rb
+++ app/helpers/congress_members_helper.rb
@@ -3,4 +3,7 @@ module CongressMembersHelper
   def congress_member_photo(member)
     image_tag "rep-photos/#{member.bioguide_id}.jpg"
   end
+  def congress_member_chamber(member)
+    "#{member.chamber}"
+  end
 end
