17b0640cb16cac1403af080a3e236f4c3c8d60b6
Vivian Brown <vivian@eff.org>
2018-02-26 15:32:59 -0800

    Integrate tweet helper with other social media links

--- app/helpers/share_link_helper.rb
+++ app/helpers/share_link_helper.rb
@@ -10,17 +10,50 @@ module ShareLinkHelper
     end
   end
 
-  def twitter_share_url
-    status = "Check Your Reps: Who Is Standing Up For Net Neutrality #{url_for_share}"
-    "https://twitter.com/intent/tweet?status=#{u(status)}"
+  def twitter_share_url(status = nil)
+    status ||= I18n.t("tweet.message.generic")
+    "https://twitter.com/intent/tweet?" +
+    {
+      status: status,
+      related: Rails.application.config.twitter_related.to_a.join(",")
+    }.to_query
   end
 
   def google_share_url
-    "https://plus.google.com/share?url=#{u(url_for_share)}"
+    "https://plus.google.com/share?#{{url: url_for_share}.to_query}"
   end
 
   def facebook_share_url
-    "https://www.facebook.com/sharer/sharer.php?u=#{u(url_for_share)}&display=popup"
+    "https://www.facebook.com/sharer/sharer.php?" +
+    {
+      u: url_for_share,
+      display: "popup"
+    }.to_query
+  end
+
+  def tweet_score_link(score)
+    message = tweet_score_message(score)
+    link_to "Tweet", twitter_share_url(message), { target: "_blank", rel: "noopener noreferrer" }
+  end
+
+  def tweet_score_message(score)
+    # 'yes' and 'no' are reserved words in YAML,
+    # so use 'for' and 'against' instead
+    position = case score.position.downcase
+    when "yes"
+      "for"
+    when "no"
+      "against"
+    else
+      "undecided"
+    end
+
+    handle = score.congress_member.twitter_handle
+
+    [
+      I18n.t("tweet.message.#{position}", handle: handle),
+      I18n.t("tweet.message.suffix")
+    ].join(" ")
   end
 
   def url_for_share
