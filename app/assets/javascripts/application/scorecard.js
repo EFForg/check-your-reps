9ca10b76729b3f331e62f3d34c8652113c009ae7
K. Stewart <ignitiant@gmail.com>
2018-02-26 19:53:06 -0800

    Underline the name of the active table sorting strategy
    https://github.com/EFForg/check-your-reps/issues/111

--- app/assets/javascripts/application/scorecard.js
+++ app/assets/javascripts/application/scorecard.js
@@ -5,3 +5,24 @@ document.addEventListener('DOMContentLoaded', () => {
   const table = document.getElementById('all-congress-members-table');
   if (table) { Tablesort(table); }
 });
+
+/* underline the active sorter */
+document.addEventListener('turbolinks:load', () => {
+  headers = Array.from(document.getElementsByTagName('th'))
+  for (i=0; i < headers.length; i++) {
+    headers[i].addEventListener("click", handleTableSort);
+  };
+});
+
+var handleTableSort = function() {
+  const headers = Array.from(document.getElementsByTagName('th'));
+
+  for (i=0; i < headers.length; i++) {
+    th = headers[i]
+    if (th.attributes.getNamedItem('aria-sort')) {
+      th.classList.add('selected');
+    } else {
+      th.classList.remove('selected');
+    }
+  };
+};
