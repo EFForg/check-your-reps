df272ac6c7a37865f41927f8d170eb8e50cb5ef5
Vivian Brown <vivian@eff.org>
2018-02-27 17:50:10 -0800

    Fix anon-stats url

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 module ApplicationHelper
   def tracking_image(piwik_id, js: false)
-    base_url = "https://anon-stats.eff.org/js?"
+    base_url = "https://anon-stats.eff.org/js/?"
     params = {
       idsite: piwik_id,
-      rec: 1,
+      rec: 1
     }
     params[:urlref] = "REFERRER" if js
     image_tag(base_url + params.to_query, style: "border:0", alt: "")
