4cda91b7d2b5c09bd898777aed835fff7fff1fe2
K. Stewart <ignitiant@gmail.com>
2018-03-28 17:08:23 -0700

    Add empty state to dashboard
    If there are no scores without positions, show a friendly message.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,11 +5,17 @@ ActiveAdmin.register_page "Dashboard" do
 
   content title: proc { I18n.t("active_admin.dashboard") } do
     panel "Congress Members without positions" do
-      table_for CongressMember.joins(:score).where(scores: {position: nil}).map do
-        column(:name) { |rep| link_to(rep.name, edit_admin_score_path(rep.score)) }
-        column :state
-        column :chamber
-        column :bioguide_id
+      missing = CongressMember.current.joins(:score).where(scores: { position: [nil, ''] })
+
+      if missing.empty?
+        text_node "All congress members have scores. Nice."
+      else
+        table_for missing.map do
+          column(:name) { |rep| link_to(rep.name, edit_admin_score_path(rep.score)) }
+          column :state
+          column :chamber
+          column :bioguide_id
+        end
       end
     end
   end
